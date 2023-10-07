5c6ef3ff87233366924f26c99bb8715bab5337e8
Shravan Asati <dev.shravan@protonmail.com>
2023-10-07 15:54:52 +0530

    update schema

--- schema.sql
+++ schema.sql
@@ -2,5 +2,11 @@ CREATE TABLE user (
   id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
-  profile_pic TEXT NOT NULL
+  profile_pic TEXT NOT NULL,
+  skills TEXT,
+  isMaintainer TEXT,
+  profession TEXT,
+  location TEXT,
+  level TEXT,
+  interests TEXT
 );
